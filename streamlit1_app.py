0bddef2bd926688e966eedac289f82598388adbd
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-21 11:57:00 +0530

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -6,6 +6,7 @@ streamlit.header('🍌Breakfast Manu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 
 
 
