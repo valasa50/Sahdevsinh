41cb908bbbf59d4e82dafd9d35fb9f9c6ce643bb
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-21 12:18:15 +0530

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -8,7 +8,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
-
+streamlit.title('🍌 Build Your Own Fruit Smoothie 🍌')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
