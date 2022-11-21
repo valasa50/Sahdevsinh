2ccdd7ec1dce6723d6ae0375d6f9cd4d2af003ca
valasa50 <117971184+valasa50@users.noreply.github.com>
2022-11-21 11:55:21 +0530

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -2,7 +2,14 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Manu')
+streamlit.header('🍌Breakfast Manu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
